0fa321c9f5e4e0291b361f56cf7b092e7f424df4
anuragkura <anuragkura@gmail.com>
2025-12-05 22:19:20 +0530

    second commit

---
+++ src/main/java/java/pre/commit/hooks/h2/SpringBootJpaH2Application.java
@@ -0,0 +1,13 @@
+package java.pre.commit.hooks.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/java/pre/commit/hooks/h2/SpringBootJpaH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package java.pre.commit.hooks.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
